c2b5eadd972e50e5f78cc39f185aa2a661a7a726
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-08-01 22:31:12 +0700

    move Enum Class to utilities

--- Project02_LogicalAgent/src/utilities.py
+++ Project02_LogicalAgent/src/utilities.py
@@ -1,11 +1,54 @@
 # utilities.py
 
-
-def read_map_file(filename):
-    with open(filename, "r") as file:
-        data = file.readlines()
-    return data
+from enum import Enum
+from typing import List, Tuple, Set, Dict
 
 
-def log_action(action):
-    print(f"Action: {action}")
+class Action(Enum):
+    FORWARD = "forward"
+    TURN_LEFT = "turn_left"
+    TURN_RIGHT = "turn_right"
+    SHOOT = "shoot"
+    GRAB = "grab"
+
+
+class Direction(Enum):
+    NORTH = "N"
+    SOUTH = "S"
+    EAST = "E"
+    WEST = "W"
+
+
+class Percept(Enum):
+    BREEZE = "B"
+    STENCH = "S"
+    SCREAM = "SC"
+    WHIFF = "WF"
+    GLOW = "GL"
+
+
+class Element(Enum):
+    AGENT = "A"
+    WUMPUS = "W"
+    GOLD = "G"
+    PIT = "P"
+    POISONOUS_GAS = "PG"
+    HEALING_POTION = "HP"
+    SAFE = None
+
+
+ELEMENT_TO_PERCEPT: Dict[Element, Percept] = {
+    Element.WUMPUS: Percept.STENCH,
+    Element.PIT: Percept.BREEZE,
+    Element.GOLD: Percept.GLOW,
+    Element.POISONOUS_GAS: Percept.WHIFF,
+    Element.AGENT: None,
+    Element.HEALING_POTION: None,
+}
+
+PERCEPT_TO_ELEMENT: Dict[Percept, Element] = {
+    Percept.STENCH: Element.WUMPUS,
+    Percept.BREEZE: Element.PIT,
+    Percept.GLOW: Element.GOLD,
+    Percept.WHIFF: Element.POISONOUS_GAS,
+}
