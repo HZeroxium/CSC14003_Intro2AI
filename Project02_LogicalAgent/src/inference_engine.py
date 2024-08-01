d1e30a50dbd14015bdccb6e16b8799e119450cc8
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-08-01 10:03:28 +0700

    update InferenceEngine

--- Project02_LogicalAgent/src/inference_engine.py
+++ Project02_LogicalAgent/src/inference_engine.py
@@ -1,20 +1,26 @@
 # inference_engine.py
 
+from environment import Percept, Element
+from knowledge_base import KnowledgeBase
+from typing import List, Tuple
+
 
 class InferenceEngine:
-    def __init__(self, knowledge_base):
+    def __init__(self, knowledge_base: KnowledgeBase):
         self.kb = knowledge_base
 
-    def infer_safe_moves(self, position):
-        # Infer which moves are safe based on current knowledge
+    def infer_safe_moves(self, position: Tuple[int, int]) -> List[Tuple[int, int]]:
         x, y = position
         moves = []
-        if self.kb.query(~self.kb.encode("P", x + 1, y)):
-            moves.append((x + 1, y))
-        if self.kb.query(~self.kb.encode("P", x - 1, y)):
-            moves.append((x - 1, y))
-        if self.kb.query(~self.kb.encode("P", x, y + 1)):
-            moves.append((x, y + 1))
-        if self.kb.query(~self.kb.encode("P", x, y - 1)):
-            moves.append((x, y - 1))
+        directions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        for i, j in directions:
+            if self.is_safe(i, j):
+                moves.append((i, j))
         return moves
+
+    def is_safe(self, x: int, y: int) -> bool:
+        return (
+            self.kb.query(-self.kb.encode(Element.PIT.value, x, y))
+            and self.kb.query(-self.kb.encode(Element.WUMPUS.value, x, y))
+            and self.kb.query(-self.kb.encode(Element.POISONOUS_GAS.value, x, y))
+        )
