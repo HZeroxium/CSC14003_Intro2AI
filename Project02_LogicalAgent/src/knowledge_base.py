267c5dd8e45344538f9f65ebf8e6b97aad2ba0c7
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-08-04 21:01:18 +0700

    refactor KnowledgeBase

--- Project02_LogicalAgent/src/knowledge_base.py
+++ Project02_LogicalAgent/src/knowledge_base.py
@@ -1,9 +1,8 @@
-# knowledge_base.py
 from pysat.formula import CNF  # type: ignore
 from pysat.solvers import Solver  # type: ignore
 from typing import List, Tuple, Dict
-from enum import Enum
 from utilities import Percept, Element, PERCEPT_TO_ELEMENT
+from enum import Enum
 
 ENCODE_MAPPING: Dict[Enum, int] = {
     Element.WUMPUS: 1,
@@ -29,7 +28,6 @@ class KnowledgeBase:
         )  # Conjunctive Normal Form of the knowledge base (list of clauses)
         self.solver = Solver(name="glucose4")  # SAT solver
         self.grid_size = grid_size
-        # self.initialize_knowledge_base()
 
     def initialize_knowledge_base(self):
         for x in range(self.grid_size):
@@ -49,56 +47,33 @@ class KnowledgeBase:
                 self.add_clause(
                     [-self.encode(elements[i], x, y), -self.encode(elements[j], x, y)]
                 )
-        pass
 
     def add_clause(self, rule: List[int]):
-        self.facts.append(
-            rule
-        )  # Add the rule to the knowledge base. Example: [B(x, y) | B(x+1, y)]
-        self.solver.add_clause(
-            rule
-        )  # Add the rule to the SAT solver. Example: [B(x, y) | B(x+1, y)]
+        self.facts.append(rule)  # Add the rule to the knowledge base
+        self.solver.add_clause(rule)  # Add the rule to the SAT solver
 
-    # Check if a literal is consistent with the knowledge base (i.e., not proven false)
     def query(self, literal: int) -> bool:
-        enum, x, y = self.decode(literal)
-        # print(f"Querying: {enum} at ({x}, {y})")
-        # Temporary addition of the negation of the literal to check consistency
         is_satisfiable = self.solver.solve(assumptions=[-literal])
         return not is_satisfiable
 
     def update(self, percepts: List[Tuple[Percept, int, int]]):
         print("===================== Knowledge Base Update =====================")
-        # Process percepts and update knowledge base
         for percept, x, y in percepts:
-            # Example: Adding the fact 'B(x, y)' if a breeze is perceived
-            if percept == Percept.BREEZE:
-                self.add_clause([self.encode(percept, x, y)])
-            if percept == Percept.STENCH:
-                self.add_clause([self.encode(percept, x, y)])
-            if percept == Percept.GLOW:
-                self.add_clause([self.encode(percept, x, y)])
-            if percept == Percept.WHIFF:
-                self.add_clause([self.encode(percept, x, y)])
-            # Add more percepts as needed
+            self.add_clause([self.encode(percept, x, y)])
         self.infer_new_knowledge()
 
     @staticmethod
     def encode(symbol: Enum, x: int, y: int) -> int:
-        # Encode a logical variable uniquely
-        hash_value = ENCODE_MAPPING[symbol] * 100 + x * 10 + y
-        return hash_value
+        return ENCODE_MAPPING[symbol] * 100 + x * 10 + y
 
     @staticmethod
     def decode(encoded: int) -> Tuple[Enum, int, int]:
-        # Decode a logical variable
-        isNegative = encoded < 0
-        if encoded < 0:
-            encoded = -encoded
+        is_negative = encoded < 0
+        encoded = abs(encoded)
         symbol = DECODE_MAPPING[encoded // 100]
         x = (encoded % 100) // 10
         y = encoded % 10
-        if isNegative:
+        if is_negative:
             return symbol, -x, -y
         return symbol, x, y
 
@@ -111,38 +86,16 @@ class KnowledgeBase:
             "===================== KnowledgeBase: Inferring New Knowledge ====================="
         )
         new_inferences = []
-        grid_size = self.grid_size
-
-        # Iterate over all possible cells in the grid
-        for x in range(grid_size):
-            for y in range(grid_size):
-                # Infer new knowledge based on the percept
-                new_inferences.extend(
-                    self.infer_from_percept(Percept.BREEZE, x, y, grid_size)
-                )
-                new_inferences.extend(
-                    self.infer_from_percept(Percept.STENCH, x, y, grid_size)
-                )
-                new_inferences.extend(
-                    self.infer_from_percept(Percept.GLOW, x, y, grid_size)
-                )
-                new_inferences.extend(
-                    self.infer_from_percept(Percept.WHIFF, x, y, grid_size)
-                )
-                # Add more percepts as needed
-
-        # Add new inferences to the knowledge base
+        for x in range(self.grid_size):
+            for y in range(self.grid_size):
+                new_inferences.extend(self.infer_from_percept(Percept.BREEZE, x, y))
+                new_inferences.extend(self.infer_from_percept(Percept.STENCH, x, y))
+                new_inferences.extend(self.infer_from_percept(Percept.GLOW, x, y))
+                new_inferences.extend(self.infer_from_percept(Percept.WHIFF, x, y))
         for inference in new_inferences:
             self.add_clause(inference)
 
-    # Helper function to infer from percept
-    def infer_from_percept(self, percept: Percept, x: int, y: int, grid_size: int):
-        # Infer new knowledge based on the percept
-        # Denote: Percept = P, Element = E
-        # Rule: P(x,y) => E(x+1,y) | E(x-1,y) | E(x,y+1) | E(x,y-1)
-        # Convert to CNF:
-        # (!P | E1 | E2 | E3 | E4) & (P | !E1) & (P | !E2) & (P | !E3) & (P | !E4)
-
+    def infer_from_percept(self, percept: Percept, x: int, y: int) -> List[List[int]]:
         percept_location_encoded = self.encode(percept, x, y)
         new_inferences = []
         if self.query(percept_location_encoded):
@@ -150,49 +103,20 @@ class KnowledgeBase:
             adjacent_cells = [
                 (i, j)
                 for i, j in adjacent_cells
-                if 0 <= i < grid_size and 0 <= j < grid_size
+                if 0 <= i < self.grid_size and 0 <= j < self.grid_size
             ]
-
             element = PERCEPT_TO_ELEMENT[percept]
-
-            percept_location_encoded = self.encode(percept, x, y)
             possible_element_locations_encoded = [
                 self.encode(element, i, j) for i, j in adjacent_cells
             ]
-
-            # Add rule (!P | E1 | E2 | E3 | E4) to the knowledge base
             rule = [-percept_location_encoded] + possible_element_locations_encoded
             print("+ Adding rule: ", self.decode_rule(rule))
             self.add_clause(rule)
-
-            # # Add rules (P | !E1) & (P | !E2) & (P | !E3) & (P | !E4) to the knowledge base
-            # for element_location_encoded in possible_element_locations_encoded:
-            #     rule = [percept_location_encoded, -element_location_encoded]
-            #     self.add_clause(rule)
-            #     if self.query(element_location_encoded):
-            #         new_inferences.append([element_location_encoded])
-            #         print("====> Inferred: ", self.decode(element_location_encoded))
-
         return new_inferences
 
 
 def main():
     knowledge_base = KnowledgeBase(grid_size=3)
-
-    # With 2x2 grid, if there is a breeze at (1, 0) and (0, 1), and (1, 1) is safe, then there is a pit at (0, 0)
-    # knowledge_base.add_clause([-knowledge_base.encode(Percept.BREEZE, 0, 1)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Percept.BREEZE, 1, 0)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Percept.BREEZE, 1, 2)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Percept.BREEZE, 2, 1)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 0, 0)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 0, 2)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 2, 0)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 2, 2)])
-    # knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 1, 1)])
-    # knowledge_base.infer_new_knowledge()
-    print(knowledge_base.solver.get_model())
-
-    # If there is a breeze at (0, 0) and there are no pit at (0, 1), then there is a pit at (1, 0)
     knowledge_base.add_clause([knowledge_base.encode(Percept.BREEZE, 0, 0)])
     knowledge_base.infer_new_knowledge()
     knowledge_base.add_clause([-knowledge_base.encode(Element.PIT, 0, 1)])
